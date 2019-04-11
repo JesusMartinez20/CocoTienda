250be0ce72eb12a61213dd19181b2d9d20c5a511
Jesús Martínez Cortez <38960100+JesusMartinez20@users.noreply.github.com>
2019-04-11 17:33:22 -0500

    Shoppingcart corrections

--- src/app/shoppingcart/shoppingcart.component.ts
+++ src/app/shoppingcart/shoppingcart.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PassingDataService } from './../Services/passing-data.service';
 import { ShoppingcartService } from './../Services/shoppingcart.service';
-import { OrdersComponent } from '../user-info/orders/orders.component';
+
 
 export interface Order{
   name: String;
@@ -24,63 +24,42 @@ export class ShoppingcartComponent implements OnInit {
   Ttotal:number;
   ped;
   x:number;
+  visible: boolean;
+  currentOrders:Order[]=[];
   constructor(private http:ShoppingcartService, private data: PassingDataService) { }
 
   ngOnInit() {
+    this.currentOrders = ShoppingcartComponent.order;
     this.data.currentMessage.subscribe(message=>{this.ped=message;this.order1=JSON.parse(this.ped);});
     console.log(this.order1);
-    this.agregar();
-    this.caltotal();
-    for(this.cont=0; this.cont<this.order.length;this.cont++){
-      this.calsubtotal(this.cont);
+
+    if(this.order1.name!=null){
+      this.agregar();
+      this.caltotal();
+      for(this.cont=0; this.cont<ShoppingcartComponent.order.length;this.cont++){
+        this.calsubtotal(this.cont);
+      }
+    }
+    if(ShoppingcartComponent.order.length==0){
+        this.visible=false;
+    }else{
+      this.visible=true;
     }
     
     
 
   }
-
-  //order:Order[]=[];
-
-  order:Order[]=[];
-  
-/*
-  order:any[]=[
-    {
-      name:"Dron",
-      quantity:6,
-      price:6000,
-      total:6000,
-      img:"https://www.fotomecanica.mx/media/catalog/product/cache/1/image/650x/040ec09b1e35df139433887a97daa66f/1/4/1479237342000_img_708264_1.jpg"
-    },
-    {
-      name:"Barbie",
-      quantity:5,
-      price:5000,
-      total:5000,
-      img:"https://yt3.ggpht.com/a-/AAuE7mDaZV0RTALrsolijo_tfHdEhSeGWCOJi56xxQ=s900-mo-c-c0xffffffff-rj-k-no"
-    },
-    {
-      name:"Max-Steel",
-      quantity:4,
-      price:4000 ,
-      total:4000,
-      img:"https://olimpica.vteximg.com.br/arquivos/ids/177568-900-900/Max-Steel-Turbo-Deslizador-Mattel.jpg?v=636782577566470000"
-    },
-    
-  ];
-*/
-
-
+  static order:Order[]=[];
 
 calsubtotal(index: number){
-  this.numsubtotal = this.order[index].quantity*this.order[index].price;
+  this.numsubtotal = ShoppingcartComponent.order[index].quantity*ShoppingcartComponent.order[index].price;
   console.log("Subtotal: ", this.numsubtotal);
   this.subtotal[index]=this.numsubtotal;
 }
 
 
   caltotal(){
-    this.numtotal = this.order.reduce((
+    this.numtotal = ShoppingcartComponent.order.reduce((
       acc,
       obj,
     ) => acc + (obj.price * obj.quantity),
@@ -90,19 +69,26 @@ calsubtotal(index: number){
   }
 
   passOrder() {
-    this.data.changeMessage(this.order);
+    this.data.changeMessage(ShoppingcartComponent.order);
   }
 
   delete(index: number){
 
 
-      if (this.order[index]) {
-        this.order.splice(index,1);
+      if (ShoppingcartComponent.order[index]) {
+        ShoppingcartComponent.order.splice(index,1);
       }
+      if(ShoppingcartComponent.order.length==0){
+          this.visible==false;
+          window.location.reload();
+      }
+      this.data.changeMessage(ShoppingcartComponent.order);      
   }
 
   agregar(){
-    this.order.push(this.order1);
+    console.log(ShoppingcartComponent.order);
+    ShoppingcartComponent.order.push(this.order1);
+        
   }
 
 
