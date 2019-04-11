b9958952435810a005b752ab5e04fc8af8c060a2
Jesús Martínez Cortez <38960100+JesusMartinez20@users.noreply.github.com>
2019-04-11 17:24:20 -0500

    Checkout almost done.

--- src/app/Services/checkout.service.ts
+++ src/app/Services/checkout.service.ts
@@ -1,24 +1,33 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class CheckoutService {
-  url="/orden";
+  url;
   constructor(private http: HttpClient) {}
 
   getMethod(){
-    return this.http.get(environment.serverUrl+this.url);
+    const headers = new HttpHeaders().append('Authorization', 'JWT ' + localStorage.getItem('token'));
+    return this.http.get(environment.serverUrl+this.url,{headers:headers});
   }
 
   postMethod(form){
-    return this.http.post(environment.serverUrl+this.url,form);
+    let authHeader = new HttpHeaders().append('Authorization', 'JWT ' + localStorage.getItem('token'));
+    authHeader = authHeader.set('Authorization', `JWT ${localStorage.getItem('token')}`)
+              .set('Content-Type', 'application/json');
+    console.log(authHeader);
+    return this.http.post(`${environment.serverUrl}${this.url}`, form, {headers: authHeader});
   }
 
   putMethod(form){
-    return this.http.put(environment.serverUrl+this.url,form);
+    const headers = new HttpHeaders().append('Authorization', 'JWT ' + localStorage.getItem('token'));
+    console.log(environment.serverUrl+this.url);
+    return this.http.put(environment.serverUrl+this.url,form,{headers:headers});
   }
 }
--- src/app/checkout/checkout-order-confirmation/checkout-order-confirmation.component.ts
+++ src/app/checkout/checkout-order-confirmation/checkout-order-confirmation.component.ts
@@ -5,7 +5,7 @@ import { PassingDataService } from './../../Services/passing-data.service';
 export interface Pedido{
   name: String;
   quantity: number;
-  price: number;
+  total: number;
   img: String;
 };
 
@@ -15,8 +15,11 @@ export interface Pedido{
   styleUrls: ['./checkout-order-confirmation.component.css']
 })
 export class CheckoutOrderConfirmationComponent implements OnInit {
+  id;
   userId=1;
   userAddress;
+  urlOrden="/pedido";
+  urlPedido="/orden"
   constructor(private http:CheckoutService,private data: PassingDataService) { }
   urlGetUserAddress="/checkout/checkout-order.php?userId="+this.userId;
   urlPostOrder="/checkout/checkout-order-post.php";
@@ -25,61 +28,70 @@ export class CheckoutOrderConfirmationComponent implements OnInit {
   ped;
   Ttotal=15000;
   ngOnInit() {
-    this.data.currentMessage.subscribe(message=>{this.ped=message;this.pedido=JSON.parse(this.ped);});
-    /*this.http.url=this.urlGetUserAddress;
-    this.http.getMethod().subscribe(d=>{this.userAddress=d;this.address=this.userAddress.fkAddress});*/
+    //this.data.currentMessage.subscribe(message=>{this.ped=message;this.pedido=JSON.parse(this.ped);});
+    //this.http.url=this.urlGetUserAddress;
+    this.http.getMethod().subscribe(d=>{this.userAddress=d;this.address=this.userAddress.fkAddress});
     
   }
   
-  pedido:Pedido[]=[];
-  /*pedido:Pedido[]=[
+  //pedido:Pedido[]=[];
+  pedido:any[]=[
     {
       name:"Dron",
-      quantity:6,
+      cantidad:6,
+      producto:1,
+      orden:this.id,
       total:6000,
-      img:"https://www.fotomecanica.mx/media/catalog/product/cache/1/image/650x/040ec09b1e35df139433887a97daa66f/1/4/1479237342000_img_708264_1.jpg"
+      img:"https://yt3.ggpht.com/a-/AAuE7mDaZV0RTALrsolijo_tfHdEhSeGWCOJi56xxQ=s900-mo-c-c0xffffffff-rj-k-no"
     },
     {
       name:"Barbie",
-      quantity:5,
+      cantidad:5,
+      producto:2,
+      orden:this.id,
       total:5000,
       img:"https://yt3.ggpht.com/a-/AAuE7mDaZV0RTALrsolijo_tfHdEhSeGWCOJi56xxQ=s900-mo-c-c0xffffffff-rj-k-no"
     },
     {
       name:"Max-Steel",
-      quantity:4,
+      cantidad:4,
+      producto:3,
+      orden:this.id,
       total:4000,
       img:"https://olimpica.vteximg.com.br/arquivos/ids/177568-900-900/Max-Steel-Turbo-Deslizador-Mattel.jpg?v=636782577566470000"
     }
-  ];*/
+  ];
 
-  order:any[]=[
+  order=
     {
       total:this.Ttotal,
-      status:"pedido",
-      fechaPedido: new Date,
-      direccion: this.address,
-      usuario: this.userId
+      estado:"Pedido",
+      direccion:1,
+      usuario:1
+
     }
-  ];
+  
   
   form;
   
   confirmOrder(){
-    this.makeOrder();
-
-    console.log(this.pedido);
-    this.form = JSON.stringify(this.pedido)
-    console.log(this.form);
-    //this.http.url=this.urlPostPedido;
-    this.http.postMethod(this.form);
+    this.makeOrder().subscribe(data => {
+      this.id= (data as any).id;
+      console.log(this.pedido);
+      this.pedido.forEach(d => d.orden = this.id);
+      console.log(this.pedido);
+      this.form = JSON.stringify(this.pedido)
+      console.log(this.form);
+      this.http.url=this.urlOrden;
+      this.http.postMethod(this.form).subscribe(r => console.log(r));
+    });
   }
 
   makeOrder(){
     this.form= JSON.stringify(this.order);
     console.log(this.form);
-    //this.http.url=this.urlPostOrder;
-    this.http.postMethod(this.form);
+    this.http.url=this.urlPedido;
+    return this.http.postMethod(this.form);
   }
 
 }
