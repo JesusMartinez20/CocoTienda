4d221e5960518a401643e2a60ef3e5cb6b7c73cf
Jesús Martínez Cortez <38960100+JesusMartinez20@users.noreply.github.com>
2019-05-09 13:03:21 -0500

    bug cart fixed.

--- src/app/shoppingcart/shoppingcart.component.ts
+++ src/app/shoppingcart/shoppingcart.component.ts
@@ -39,6 +39,7 @@ export class ShoppingcartComponent implements OnInit {
     //this.data.currentMessage.subscribe(message=>{this.ped=message;this.order1=JSON.parse(this.ped);});
     this.order1=this.currentOrders[this.currentOrders.length-1];
     console.log(this.order1);
+    console.log(this.currentOrders);
 
     if(this.order1.name!=null){
       for(this.cont=0; this.cont<this.currentOrders.length-1;this.cont++){///Nuevo
@@ -58,7 +59,7 @@ export class ShoppingcartComponent implements OnInit {
         this.caltotal();
       }
       this.repetido=false;
-      for(this.cont=0; this.cont<ShoppingcartComponent.order.length;this.cont++){
+      for(this.cont=0; this.cont<this.currentOrders.length;this.cont++){
         this.calsubtotal(this.cont);
       }
     }
@@ -75,23 +76,23 @@ export class ShoppingcartComponent implements OnInit {
   static order2:any[]=[];///Nuevo
 
 calsubtotal(index: number) {
-  this.numsubtotal = ShoppingcartComponent.order[index].cantidad*ShoppingcartComponent.order[index].precio;
-  console.log("Subtotal: ", this.numsubtotal,ShoppingcartComponent.order[index].cantidad);
+  this.numsubtotal = this.currentOrders[index].cantidad*this.currentOrders[index].precio;
+  console.log("Subtotal: ", this.numsubtotal,this.currentOrders[index].cantidad);
   this.subtotal[index]=this.numsubtotal;
-  ShoppingcartComponent.order[index].total=this.subtotal[index];
+  this.currentOrders[index].total=this.subtotal[index];
 }
 
 
   caltotal(){
-    this.numtotal = ShoppingcartComponent.order.reduce((
+    this.numtotal = this.currentOrders.reduce((
       acc,
       obj,
     ) => acc + (obj.precio * obj.cantidad),
     0);
     console.log("Total: ", this.numtotal)
     this.Ttotal=this.numtotal;
-    for(this.cont=0; this.cont<ShoppingcartComponent.order.length;this.cont++){
-      ShoppingcartComponent.order[this.cont].ttotals=this.Ttotal;
+    for(this.cont=0; this.cont<this.currentOrders.length;this.cont++){
+      this.currentOrders[this.cont].ttotals=this.Ttotal;
     }
     
   }
@@ -117,28 +118,16 @@ calsubtotal(index: number) {
     
   }
 
-  delete(index:number){/*
-    console.log(this.order);
-    let form = JSON.stringify(this.order)
-    console.log(form);
-    this.http.url=this.urlD;
-    this.http.deleteMethod(form);*/
-    /*let i=this.order.findIndex();
-    if(i==-1){
-      this.order.push()
-    }else{
-      this.order.splice(i,1,{name:this.order1.name})
-    }
-*/
+  delete(index:number){
 
-      if (ShoppingcartComponent.order[index]) {
-        ShoppingcartComponent.order.splice(index,1);
+      if (this.currentOrders[index]) {
+        this.currentOrders.splice(index,1);
       }
-      if(ShoppingcartComponent.order.length==0){
+      if(this.currentOrders.length==0){
           this.visible==false;
           window.location.reload();
       }
-      this.data.changeMessage(ShoppingcartComponent.order);      
+      this.data.changeMessage(this.currentOrders);      
   }
 
   agregar(){
