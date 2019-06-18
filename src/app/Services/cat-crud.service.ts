91e4593431ff82d7d54d5768e94d9ef9f770136b
ClaudiaPC3 <chamg7@hotmail.com>
2019-06-18 14:09:29 -0500

    Service Creation for crud of category

---
+++ src/app/Services/cat-crud.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from './../../../src/environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CatCrudService {
+  url; 
+  constructor(private http: HttpClient) { }
+  getMethod(): Observable<any>{
+    const headers = new HttpHeaders().append('Authorization', 'JWT ' + localStorage.getItem('token'));
+    return this.http.get(environment.serverUrl+this.url, {headers: headers});
+  }
+
+  putMethod(form): Observable<any>{
+    const headers = new HttpHeaders().append('Authorization', 'JWT ' + localStorage.getItem('token'));
+    return this.http.put(environment.serverUrl+this.url, form, {headers: headers});  
+  }
+
+  postMethod(form): Observable<any>{
+    const headers = new HttpHeaders().append('Authorization', 'JWT ' + localStorage.getItem('token'));
+    return this.http.post(environment.serverUrl+this.url,form, {headers: headers});
+  }
+
+  deleteMethod(form): Observable<any>{
+    const headers = new HttpHeaders().append('Authorization', 'JWT ' + localStorage.getItem('token'));
+    return this.http.request('delete',environment.serverUrl+this.url, {
+      headers: headers,
+      body: form
+    });
+  }
+
+}
